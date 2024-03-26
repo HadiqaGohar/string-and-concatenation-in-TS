07a747ee4cfe85aa17d9d9cd7ae15beaa6606299
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-26 04:37:19 -0700

    concatenation

---
+++ main.js
@@ -0,0 +1,15 @@
+//PRACTICE QUESTION 
+//STRING AND CONCATINATION
+//QUESTION 1
+var firstName = "Hadiqa";
+var lastName = "Gohar";
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+//QUESTION 2
+var fruit = ["Mango", "Apple", "Peach", "Grapes", "Banana"];
+var message = "I like";
+console.log(message + ' ' + fruit[0]);
+console.log(message + ' ' + fruit[1]);
+console.log(message + ' ' + fruit[2]);
+console.log(message + ' ' + fruit[3]);
+console.log(message + ' ' + fruit[4]);
---
+++ main.ts
@@ -0,0 +1,19 @@
+//PRACTICE QUESTION 
+//STRING AND CONCATINATION
+
+//QUESTION 1
+
+let firstName : string = "Hadiqa";
+let lastName : string = "Gohar";
+let fullName : string = firstName + ' ' + lastName;
+console.log (fullName);
+
+//QUESTION 2
+
+let fruit : string [] = ["Mango","Apple","Peach","Grapes","Banana"];
+let message : string = "I like";
+console.log(message + ' ' + fruit[0]);
+console.log(message + ' ' + fruit[1]);
+console.log(message + ' ' + fruit[2]);
+console.log(message + ' ' + fruit[3]);
+console.log(message + ' ' + fruit[4]);
